d5c806c0a76e8ee34d7918254219b336ba74eeb9
Hussian Shaik <atxwebdesigner1@gmail.com>
2019-09-30 15:33:47 +0530

    added the forgot password basic design screen

--- app/navigation/AppNavigation.ts
+++ app/navigation/AppNavigation.ts
@@ -14,12 +14,14 @@ import { createStackNavigator } from "react-navigation-stack"
 import Splash from "../screens/splash"
 import Register from "../screens/register"
 import Login from "../screens/login"
+import ForgotPassword from "../screens/forgot-password"
 
 const stackNav = createStackNavigator(
   {
     Splash: { screen: Splash },
     Register: { screen: Register },
     Login: { screen: Login },
+    ForgotPassword: { screen: ForgotPassword },
   },
   {
     initialRouteName: "Splash",
