a6c44f8523eb3320c0e0392df94cef8a7a072d72
Hussian Shaik <atxwebdesigner1@gmail.com>
2019-10-04 16:48:32 +0530

    fixed the drawer issue

--- app/screens/map/index.tsx
+++ app/screens/map/index.tsx
@@ -3,90 +3,94 @@ import { View } from "react-native"
 import { NavigationScreenProp, NavigationState } from "react-navigation"
 import styles from "./styles"
 
-import MapView from 'react-native-maps';
+import MapView from "react-native-maps"
 import { Header } from "../../components/header"
-import { Drawer } from 'native-base';
+import { Drawer } from "native-base"
 
 import SideBar from "../side-bar/index"
 import { GoldBarView } from "../../components/goldBar"
 
 interface Props {
-    navigation: NavigationScreenProp<NavigationState>
+  navigation: NavigationScreenProp<NavigationState>
+}
+interface MapScreen {
+  drawer: any
+  state: any
+  region: any
+  isOpen: boolean
 }
 
-class MapScreen extends Component<Props, {}> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            region: {
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+class MapScreen extends Component<Props, MapScreen, {}> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      isOpen: false,
+    }
+  }
+  closeDrawer() {
+    this.drawer._root.close()
+  }
+
+  onRegionChange(region) {
+    this.setState({ region: region })
+  }
+
+  onLeft() {
+    if (this.state.isOpen) {
+      this.drawer._root.close()
+      this.setState({
+        isOpen: false,
+      })
+    } else {
+      this.drawer._root.open()
+      this.setState({
+        isOpen: true,
+      })
+    }
+  }
+
+  render() {
+    const { navigation } = this.props
+    return (
+      <View style={{ flex: 1 }}>
+        <Header
+          style={styles.header}
+          headerText={"STAY TUNE"}
+          titleStyle={styles.headerTitle}
+          leftIcon={"menu"}
+          onLeftPress={this.onLeft.bind(this)}
+        />
+        <GoldBarView />
+        <View style={{ flex: 1, overflow: "hidden" }}>
+          <Drawer
+            ref={ref => {
+              this.drawer = ref
+            }}
+            content={
+              <SideBar
+                navigation={this.props.navigation}
+                // onCloseMenu={params => this.closeDrawer(params)}
+              />
             }
-        }
-    }
-
-    // getInitialState() {
-    //     return {
-    //         region: {
-    //             latitude: 37.78825,
-    //             longitude: -122.4324,
-    //             latitudeDelta: 0.0922,
-    //             longitudeDelta: 0.0421,
-    //         },
-    //     };
-    // }
-
-    closeDrawer() {
-        this.drawer._root.close();
-    }
-
-    onRegionChange(region) {
-        this.setState({ region: region });
-    }
-
-    onLeft() {
-        this.drawer._root.open();
-    }
-
-    render() {
-        const { navigation } = this.props
-        return (
-
-            <View style={{ flex: 1 }}>
-                <Header
-                    style={styles.header}
-                    headerText={"STAY TUNE"}
-                    titleStyle={styles.headerTitle}
-                    leftIcon={"menu"}
-                    onLeftPress={this.onLeft.bind(this)} />
-                <GoldBarView />
-                <View style={{ flex: 1, overflow: 'hidden' }}>
-                    <Drawer
-                        ref={ref => {
-                            this.drawer = ref;
-                        }}
-                        content={
-                            <SideBar
-                                navigation={this.props.navigation}
-                            // onCloseMenu={params => this.closeDrawer(params)}
-                            />
-                        }
-                        onClose={() => this.closeDrawer()}
-                    >
-                        <MapView
-                            style={{ flex: 1 }}
-                            region={this.state.region}
-                            onRegionChange={this.onRegionChange.bind(this)}
-                            showsUserLocation={true}
-                        />
-                    </Drawer>
-                </View>
-            </View >
-
-        )
-    }
+            onClose={() => this.closeDrawer()}
+          >
+            <MapView
+              style={{ flex: 1 }}
+              region={this.state.region}
+              onRegionChange={this.onRegionChange.bind(this)}
+              showsUserLocation={true}
+            />
+          </Drawer>
+        </View>
+      </View>
+    )
+  }
 }
 
 export default MapScreen
