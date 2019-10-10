4c48d6d407d89f022607f74f82967b7d616f7694
Mounika <atxwebdesigner1@gmail.com>
2019-10-10 17:50:10 +0530

    pull request

--- app/redux/actions/user.ts
+++ app/redux/actions/user.ts
@@ -1,62 +1,55 @@
-
-import resToBody from '../resToBody/resToBody';
+import resToBody from "../resToBody/resToBody"
 export const SIGN_UP = "SIGN_UP"
 export const LOGIN = "LOGIN"
 
 const STAYTUNELIVEURL = "https://staytune.austinconversionoptimization.com/"
 
 export function signUp(fullName, email, password) {
-    return async dispatch => {
-        const res = await fetch(
-            STAYTUNELIVEURL + `users`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "fullname": fullName,
-                    "email": email,
-                    "password": password
-                })
-            }
-        );
-        const body = await resToBody(res);
-        console.log("body_123:", body)
-        return dispatch({
-            type: SIGN_UP,
-            payload: body
-        });
-    }
+  return async dispatch => {
+    const res = await fetch(STAYTUNELIVEURL + `users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fullname: fullName,
+        email: email,
+        password: password,
+      }),
+    })
+    const body = await resToBody(res)
+    console.log("body_123:", body)
+    return dispatch({
+      type: SIGN_UP,
+      payload: body,
+    })
+  }
 }
 
 export function Login(email, password) {
-    return async dispatch => {
-        const res = await fetch(
-            STAYTUNELIVEURL + `users/login`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "email": email,
-                    "password": password
-                })
-            }
-        );
-        const body = await resToBody(res);
-        console.log("body_123:", body)
-        return dispatch({
-            type: LOGIN,
-            payload: body
-        });
-    }
+  return async dispatch => {
+    const res = await fetch(STAYTUNELIVEURL + `users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+    const body = await resToBody(res)
+    console.log("body_123:", body)
+    return dispatch({
+      type: LOGIN,
+      payload: body,
+    })
+  }
 }
 
 export default {
-    signUp,
-    Login,
-    SIGN_UP,
-    LOGIN
+  signUp,
+  Login,
+  SIGN_UP,
+  LOGIN,
 }
