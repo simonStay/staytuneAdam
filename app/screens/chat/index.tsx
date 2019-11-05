345bc989c578d71d34d2a29e83b9eeb662f94f58
Hussian Shaik <atxwebdesigner1@gmail.com>
2019-11-05 18:46:06 +0530

    Added the basic chatboat

--- app/screens/chat/index.tsx
+++ app/screens/chat/index.tsx
@@ -1,28 +1,70 @@
-import React, { Component } from "react"
-import { View } from "react-native"
-import { NavigationScreenProp, NavigationState, ScrollView } from "react-navigation"
-import styles from "./styles"
-import { Text } from "../../components/text"
+import React from 'react';
+import inspirationalQuotes from 'inspirational-quotes';
+import { GiftedChat } from 'react-native-gifted-chat';
 
-interface Props {
-  navigation: NavigationScreenProp<NavigationState>
-}
-interface UserInformation { }
+let id1 = 1;
+let id2 = id1 + 1;
+export default class Chat extends React.Component {
+  state = {
+    messages: []
+  };
 
-class Chat extends Component<Props, UserInformation> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {}
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: id1,
+          text: 'welcome to chat screen',
+          createdAt: new Date(),
+          user: {
+            _id: id2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        }
+      ]
+    });
   }
+
+  onSend(messages = []) {
+    // console.log("message", JSON.stringify(messages));
+    id1 = id2 + 1;
+    id2 = id1 + 1;
+    this.setState(
+      previousState => ({
+        messages: GiftedChat.append(previousState.messages, messages)
+      }),
+      () => {
+        setTimeout(() => {
+          const message = [
+            {
+              _id: id1,
+              text: inspirationalQuotes.getRandomQuote(),
+              createdAt: new Date(),
+              user: {
+                _id: id2,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any'
+              }
+            }
+          ];
+          this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, message)
+          }));
+        }, 3000);
+      }
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <Text style={styles.initialText}>CHAT COMING SOON....</Text>
-        </ScrollView>
-      </View>
-    )
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+    );
   }
 }
-
-export default Chat
